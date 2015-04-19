4dad5dc99dad7fe25a68efcc0dd8735c2f598580
Josh Hamilton <joshamilton@gmail.com>
2015-04-19 13:05:57 -0500

    Python codes for initial graph analysis.
    These scripts read in the adjacency lists and perform some preliminary
    analysis the directed graphs. “See 2015-04-17 Graph Analysis” in
    notebook.

---
+++ graphFunctions.py
@@ -0,0 +1,48 @@
+###############################################################################
+# graphFunctions.py
+# Copyright (c) 2015, Joshua J Hamilton and Katherine D McMahon
+# Affiliation: Department of Bacteriology
+#              University of Wisconsin-Madison, Madison, Wisconsin, USA
+# URL: http://http://mcmahonlab.wisc.edu/
+# All rights reserved.
+################################################################################
+# Set of functions for manipulating SBML files
+################################################################################
+
+import networkx as nx
+import numpy as np
+from matplotlib import pyplot
+
+def getGraphStats(graph):
+    statRow = [0]*4
+    statRow[0] = graph.number_of_nodes()
+    statRow[1] = graph.number_of_edges()
+    myConComp = nx.connected_components(graph)
+    myConCompList = sorted(myConComp, key = len, reverse=True)
+    statRow[2] = len(myConCompList)
+    statRow[3] = len(myConCompList[0])
+    return statRow
+    
+def plotGraphStats(graphStatArray):
+    myWeight = np.ones_like(graphStatArray[:,0]) / float(len(graphStatArray[:,0]))
+    pyplot.figure(1)
+    pyplot.hist(graphStatArray[:,0], weights=myWeight)
+    pyplot.xlabel('Total Nodes')
+    pyplot.ylabel('Fraction of Graphs')
+
+# Histogram of number of components
+    myWeight = np.ones_like(graphStatArray[:,2]) / float(len(graphStatArray[:,2]))
+    pyplot.figure(2)
+    pyplot.hist(graphStatArray[:,2], weights=myWeight)
+    pyplot.xlabel('Number of Components')
+    pyplot.ylabel('Fraction of Graphs')
+
+# Histogram of largest component size
+    myArray = np.true_divide(graphStatArray[:,3], graphStatArray[:,0]);
+    myWeight = np.ones_like(myArray) / float(len(myArray))
+    pyplot.figure(3)
+    pyplot.hist(myArray, weights=myWeight)
+    pyplot.xlabel('Fraction of Nodes in Largest Component')
+    pyplot.ylabel('Fraction of Graphs')
+    
+    return
--- reverseEcology.py
+++ reverseEcology.py
@@ -16,12 +16,13 @@ import networkx as nx
 import numpy as np
 import os
 import pandas
+from matplotlib import pyplot
 
 # Import additional functions which are found in relevant scripts
-import sbmlFunctions
+import sbmlFunctions as sf
+import graphFunctions as gf
 
 # Retrieve listing of model subdirectories
-
 dirList = filter(os.path.isdir, glob.glob('*'))
 numSubDir = len(dirList)
 
@@ -35,7 +36,7 @@ for curDir in dirList:
     model = cobra.io.read_sbml_model(curDir+'/'+curDir+'Balanced.xml')
     model.description = curDir;
 # Read model statistics
-    modelStatArray[count:] = sbmlFunctions.getModelStats(model)
+    modelStatArray[count:] = sf.getModelStats(model)
     sbmlFunctions.adjacencyListFromModel(model)
     count = count + 1
 
@@ -43,3 +44,48 @@ for curDir in dirList:
 colLabels = ['Genes', 'Metabolites', 'Reactions']
 modelStatDF = pandas.DataFrame(modelStatArray, columns=colLabels, index=dirList) 
 modelStatDF.to_csv('ModelStatistics.txt')
+
+#%%
+# Import adjacencylists as graphs. Retrieve graph statistics.
+graphStatArray = np.empty([numSubDir, 4], dtype = int)
+
+count = 0
+for curDir in dirList:
+    print 'Processing directory', count+1, 'of', numSubDir, ':', curDir
+# Construct adjacency list and convert to graph object
+    myGraph = nx.read_adjlist(curDir+'/'+curDir+'AdjList.txt', delimiter='\t', 
+                              create_using=nx.Graph())
+    myDiGraph = nx.read_adjlist(curDir+'/'+curDir+'AdjList.txt', delimiter='\t', 
+                              create_using=nx.DiGraph())
+# Read graph statistics                       
+    graphStatArray[count:] = gf.getGraphStats(myGraph)
+    count = count + 1
+
+colLabels = ['Nodes', 'Edges', 'TotalComponents', 'Size of Largest']
+graphStatDF = pandas.DataFrame(graphStatArray, columns=colLabels, index=dirList) 
+graphStatDF.to_csv('GraphStatistics.txt')
+
+# Make some graphs
+gf.plotGraphStats(graphStatArray)
+
+#%%
+
+# To identify the most highly connected metabolites we will:
+curDir = 'TBhypometabat3838' #smallest
+#curDir - 'TBhypometabat3815' #largest
+myGraph = nx.read_adjlist(curDir+'/'+curDir+'AdjList.txt', delimiter='\t', 
+                              create_using=nx.Graph())
+myCent = nx.degree_centrality(myGraph)
+
+import operator
+myCentSorted = sorted(myCent.items(), key=operator.itemgetter(1), reverse=True)
+myRank = np.linspace(1,len(myCentSorted), len(myCentSorted))
+
+# Plot the data
+pyplot.scatter(myRank, myCentSorted)
+# Not plotting properly b/c myCentSorted is a list of lists. How do I extract
+# the second column to plot it?
+
+
+# Loop over all graphs. For each node, check if node has already been found. If
+# it has a higher connectivity, then update the hash.
