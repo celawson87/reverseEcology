705239809d1cd0926aaa351588907b6947bd03cf
Josh Hamilton <joshamilton@gmail.com>
2015-05-27 14:34:28 -0500

    Tweaking of dendrogram.
    analyzeSeedSets.py updated to replace metabolite SEED IDs with common
    names. External data file metabMap.csv contains a manual mapping of
    SEED IDs to common names. iPython Notebook updated to include code for
    dendogram.

--- Python/analyzeSeedSets.py
+++ Python/analyzeSeedSets.py
@@ -27,8 +27,10 @@ import numpy as np
 # name should match the genome name, and should contain a genome-scale model
 # of the genome. The model should be named 'genomeNameBalanced.xml'.
 # summaryStatsDir: directory for storing summary statistics
+# externalDataDir: 
 processedDataDir = 'ProcessedModelFiles'
 summaryStatsDir = 'DataSummaries'
+externalDataDir = 'ExternalData'
 
 # Retrieve list of genomes to process by examing the contents of 
 # 'processedDataDir', ignoring hidden folders. Subsequent computations are 
@@ -79,8 +81,9 @@ writer = csv.writer(seedCounts, lineterminator='\n')
 writer.writerows(seedMetabCountList)
 seedCounts.close()
 
-# Turns out a plot of (name, frequency) pairs isn't very useful because
-# there are so many of them!
+# Create a scatter plot of (name, frequency) pairs for all metabolites in the
+# seed sets. Turns out such a plot isn't very useful because there are so many 
+# metabolites!
 #x = np.linspace(1, len(seedMetabCount), len(seedMetabCount))
 #y = zip(*seedMetabCountList)[1]
 #labels = zip(*seedMetabCountList)[0]
@@ -93,14 +96,12 @@ seedCounts.close()
 #pyplot.xlabel('Metabolite')
 #pyplot.ylabel('Frequency as a Seed')
 
-
-#%% Clustering.
-
-# A data frame would be a great way to visualize the data. Then we can extract
-# the columns for clustering.
+#%% Consolidation seed weights into a single data frame.
+# This code snippet reads in the seed metabolites and their weights for each 
+# genome. The data is read into a dataframe and then written to file. Structure:
 # Rows: metabolites
 # Columns: graphs
-# Entries: (un)weighted seed set values
+# Entries: unweighted seed set values
 
 # Import various packages
 from pandas import DataFrame, read_csv
@@ -118,67 +119,97 @@ for curDir in dirList:
 # Replace all the NaN values with zeros
 seedMatrixDF.fillna(0, inplace=True)
 
+# Append a new column containing common names associated with metabolite IDs.
+# The file metabMap.csv was created manually from the seed database, and should
+# be updated to reflect the particulars of your data set.
+namesDF = pandas.read_csv('../'+externalDataDir+'/'+'metabMap.csv', names=['Metabolite','CommonName'])
+seedMatrixDF = pandas.merge(seedMatrixDF, namesDF, how='outer', on="Metabolite")
+
+# Rearrange the order of the columns so that the common name is in front
+newOrder = seedMatrixDF.columns.tolist()
+newOrder = newOrder[-1:] + newOrder[:-1]
+seedMatrixDF = seedMatrixDF[newOrder]
+    
+# Export the matrix of seed weights
+pandas.DataFrame.to_csv(seedMatrixDF, '../'+summaryStatsDir+'/'+'seedMatrixWeighted.csv')
+
 
 #%% Clustering and Visualization
 
+# This code snippet creates a dendrogram of the seed sets. The genomes are
+# clustered based on weighted vectors of seed sets, using the euclidean
+# distance and UPGMA (average linkage) clustering. A second dendogram is
+# constructed based on metabolite weights across genomes. Then, the matrix
+# of seed weights is reordered to reflect the order of the dendograms and is
+# visualized.
+
+# Import Python packages.
 from scipy import spatial
 from scipy import cluster
 import scipy
 import scipy.cluster.hierarchy as sch
 
-# Export the matrix of seed weights
-pandas.DataFrame.to_csv(seedMatrixDF, '../'+summaryStatsDir+'/'+'seedMatrixWeighted.csv')
-
 # Python clustering algorithms require the data to be an ndarray, with each
 # row corresponding to a set of observations. Thus, calculation of the linkage
 # for the genomes must be performed on the transpose.
-seedMatrix=DataFrame.as_matrix(seedMatrixDF.drop('Metabolite', 1))
-seedMatrixT=np.transpose(DataFrame.as_matrix(seedMatrixDF.drop('Metabolite', 1)))
+seedMatrix=DataFrame.as_matrix(seedMatrixDF.drop(['CommonName','Metabolite'], 1))
+seedMatrixT=np.transpose(DataFrame.as_matrix(seedMatrixDF.drop(['CommonName','Metabolite'], 1)))
 
-#%% Clustering.
-
+# Create a figure to display the seed weights and dendograms.
 fig = pyplot.figure(figsize=(7.5, 61))
 
-# Compute and plot dendogram for genomes, which will be above the graph. This
-# dendogram is for the genomes.
+# Compute and plot dendogram for genomes, which will be above the graph.
+# Define the size of the dendogram
 ax1 = fig.add_axes([0.05,0.9,0.8,0.05], frame_on=False)
+# Compute the linkage matrix
 Y = sch.linkage(seedMatrixT, method='complete', metric='euclidean')
+# Compute the dendogram
 Z1 = sch.dendrogram(Y, distance_sort='True')
+# No tick marks along axes
 ax1.set_xticks([])
 ax1.set_yticks([])
 
-# Compute and plot second dendrogram, which will be beside the graph. This
-# dendogram is for the metabolites.
+# Compute and plot dendogram for metabolites which will be beside the graph. 
+# Define the size of the dendogram
 ax2 = fig.add_axes([0,0.1,0.05,0.7], frame_on=False)
+# Compute the linkage matrix
 Y = sch.linkage(seedMatrix, method='average', metric='euclidean')
+# Compute the dendogram
 Z2 = sch.dendrogram(Y, orientation='right')
+# No tick marks along axes
 ax2.set_xticks([])
 ax2.set_yticks([])
 
-
-# Plot distance matrix.
+# Plot the matrix of seed weights.
+# Define the size of the plot
 axmatrix = fig.add_axes([0.05,0.1,0.8,0.8])
+# Determine ordering of the dendograms and rearrange the weight matrix
 idx1 = Z1['leaves']
 idx2 = Z2['leaves']
 seedMatrix = seedMatrix[:,idx1]
 seedMatrix = seedMatrix[idx2,:]
+# Plot the weight matrix
 im = axmatrix.matshow(seedMatrix, aspect='auto', origin='lower')
+# No tick marks along axes
 axmatrix.set_xticks([])
 axmatrix.set_yticks([])
 
+# Add genome names to the bottom axis
 axmatrix.set_xticks(range(len(seedMatrixT)))
 axmatrix.set_xticklabels([dirList[i] for i in idx1], minor=False)
 axmatrix.xaxis.set_label_position('bottom')
 axmatrix.xaxis.tick_bottom()
 pyplot.xticks(rotation=-90, fontsize=8)
 
+# Add metabolite names to the right axis
 axmatrix.set_yticks(range(len(seedMatrix)))
-axmatrix.set_yticklabels([seedMatrixDF['Metabolite'][i] for i in idx2], minor=False)
+axmatrix.set_yticklabels([seedMatrixDF['CommonName'][i] for i in idx2], minor=False)
 axmatrix.yaxis.set_label_position('right')
 axmatrix.yaxis.tick_right()
+pyplot.yticks(fontsize=8)
 
 # Plot colorbar.
-axcolor = fig.add_axes([0.97,0.1,0.03,0.8])
+axcolor = fig.add_axes([0.97,0.9,0.03,0.05])
 pyplot.colorbar(im, cax=axcolor)
 fig.show()
 fig.savefig('../'+summaryStatsDir+'/'+'seedSetDendrogram.png')
